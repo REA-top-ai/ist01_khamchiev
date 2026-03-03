8adbad8d71b092eaf84bde0a39edf182d670fdc7
рахим <sellman1ks4@gmail.com>
2026-03-03 20:14:13 +0300

    Add files via upload

---
+++ 03032026_ХамчиевРА-Г.py
@@ -0,0 +1,25 @@
+logs = [
+    "2025-02-01 10:15:33|INFO|user=anna action=login status=success ip=10.0.0.1",
+    "2025-02-01 10:17:10|ERROR|user=bob action=payment status=fail amount=120",
+    "2025-02-01 10:20:01|INFO|user=anna action=logout status=success",
+    "2025-02-01 10:22:45|WARNING|user=anna action=payment status=fail amount=300",
+    "2025-02-01 10:30:12|ERROR|user=tom action=login status=fail ip=10.0.0.5"
+]
+
+parsed_logs = []
+
+for line in logs:
+    parts = line.split("|")
+    date = parts[0]
+    level = parts[1]
+    message = parts[2]
+    fields = message.split(" ")
+
+    data = {}
+
+    for f in fields:
+        kv = f.split("=")
+        data[kv[0]] = kv[1]
+        data["date"] = date
+        data["level"] = level
+        parsed_logs.append(data)
